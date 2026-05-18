f6e4b2f0ae0e6b5001933711391a01da09756733
data8320-24 <sksksmssksml@gmail.com>
2026-05-19 08:59:58 +0900

    두 수의 차 구하기

---
+++ src/main/java/org/example/level0/DivideNumber_05.java
@@ -0,0 +1,25 @@
+package org.example.level0;
+
+public class DivideNumber_05 {
+    public static void main(String[] args) {
+        /*
+
+        두 수의 차 구하기
+
+        정수 num1과 num2가 주어질 때, num1에서 num2를 뺀 값을
+        return하도록 soltuion 함수를 완성해주세요.
+         */
+
+        int num1 = 2;
+        int num2 = 3;
+
+        System.out.println(solution(num1, num2));
+    }
+
+    static int solution(int num1, int num2) {
+
+        int answer = num1 - num2;
+
+        return answer;
+    }
+}
