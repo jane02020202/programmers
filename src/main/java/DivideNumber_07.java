166cbc4f48c79cefe7bd7b9d02c35a6fd101ae96
data8320-24 <sksksmssksml@gmail.com>
2026-05-19 09:24:54 +0900

    두 수의 합 구하기

---
+++ src/main/java/DivideNumber_07.java
@@ -0,0 +1,24 @@
+public class DivideNumber_07 {
+    public static void main(String[] args) {
+
+        /*
+
+        두 수의 합 구하기
+
+        정수 num1과 num2가 주어질 때, num1과 num2의 합을
+        return하도록 soltuion 함수를 완성해주세요.
+         */
+
+        int num1 = 2;
+        int num2 = 3;
+
+        System.out.println(solution(num1, num2));
+    }
+
+    static int solution(int num1, int num2) {
+
+        int answer = num1 + num2;
+
+        return answer;
+    }
+}
