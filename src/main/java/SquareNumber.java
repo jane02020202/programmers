445df20447060f63ecbe30ece2412242c3d5e977
data8320-24 <sksksmssksml@gmail.com>
2026-07-13 10:03:41 +0900

    제곱수 판별하기

---
+++ src/main/java/SquareNumber.java
@@ -0,0 +1,26 @@
+public class SquareNumber {
+
+    /*
+    제곱수 판별하기
+
+    어떤 자연수를 제곱했을 때 나오는 정수를 제곱수라고 합니다. 정수 n이 매개변수로 주어질 때,
+    n이 제곱수라면 1을 아니라면 2를 return하도록 solution 함수를 완성해주세요.
+     */
+
+    public static void main(String[] args) {
+
+        int n = 144;
+
+        System.out.println(solution(n));
+
+    } static int solution(int n) {
+
+        double sqrt = Math.sqrt(n);
+
+        if (sqrt == (int) sqrt) {
+            return 1;
+        } else {
+            return 2;
+        }
+    }
+}
