d5871877f2d4c29fd871f825ea129689e258baf0
data8320-24 <sksksmssksml@gmail.com>
2026-07-15 21:07:52 +0900

    세균 두배 증식

---
+++ src/main/java/DoubleBacteria.java
@@ -0,0 +1,26 @@
+public class DoubleBacteria {
+
+    /*
+    세균 증식
+
+    어떤 세균은 1시간에 두배만큼 증식한다고 합니다. 처음 세균의 마리수 n과 경과한 시간
+    t가 매개변수로 주어질 때 t시간 후 세균의 수를 return하도록 solution 함수를 완성해주세요.
+     */
+
+    public static void main(String[] args) {
+
+        int n = 2;
+        int t = 10;
+
+        System.out.println(solution(n, t));
+
+    } static int solution(int n, int t) {
+
+        int answer = n;
+
+        for(int i = 0; i < t; i++) {
+            answer *= 2;
+        }
+        return answer;
+    }
+}
